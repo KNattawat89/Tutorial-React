d043770aec97fcca3cf8d60a7614e688400822f9
Nattawat Teaiwtan <Nao.Nait@gmail.com>
2022-04-25 15:43:37 +0700

    Interactive component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render() {
       return (
-        <button className="square">
-          {this.props.value}
-        </button>
+        <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
       );
     }
   }
